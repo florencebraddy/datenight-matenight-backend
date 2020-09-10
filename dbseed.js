dc5c0e94741843c952f49c39a89c6ed286531d9c
Florence Braddy <florencebraddy@hotmail.com>
2020-09-09 20:20:17 -0400

    user database created, .env added

---
+++ dbseed.js
@@ -0,0 +1,86 @@
+require("dotenv").config();
+const sql = require("mysql2/promise");
+
+const pool = sql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
+
+(async function testConnection() {
+  try {
+    const conn = await pool.getConnection();
+    console.log("Connection Successful");
+    conn.release();
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+(async function createUserTable() {
+  try {
+    const conn = await pool.getConnection();
+
+    conn.query("CREATE DATABASE IF NOT EXISTS datenight");
+    conn.query("USE datenight");
+
+    const userDB = await conn.query(
+      "CREATE TABLE IF NOT EXISTS user (username VARCHAR(255) UNIQUE NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, user_location VARCHAR(255) NOT NULL, profile_picture VARCHAR(255) NOT NULL, PRIMARY KEY(username) )"
+    );
+    console.log(userDB);
+    conn.release();
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+// (async function createDateNightTable() {
+//   try {
+//     const conn = await pool.getConnection();
+
+//     conn.query("CREATE DATABASE IF NOT EXISTS datenight");
+//     conn.query("USE datenight");
+
+//     const datenightDB = await conn.query(
+//       "CREATE TABLE IF NOT EXISTS activity (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(255) UNIQUE NOT NULL, description VARCHAR(3000) NOT NULL, actitivity_location VARCHAR(255) NOT NULL, price VARCHAR(255) NOT NULL, PRIMARY KEY(id) )"
+//     );
+//     console.log(datenightDB);
+//     conn.release();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
+
+// (async function createActitivityTable() {
+//   try {
+//     const conn = await pool.getConnection();
+
+//     conn.query("CREATE DATABASE IF NOT EXISTS datenight");
+//     conn.query("USE datenight");
+
+//     const activityDB = await conn.query(
+//       "CREATE TABLE IF NOT EXISTS activity (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(255) UNIQUE NOT NULL, description VARCHAR(3000) NOT NULL, activity_location VARCHAR(255) NOT NULL, price VARCHAR(255) NOT NULL, PRIMARY KEY(id) )"
+//     );
+//     console.log(activityDB);
+//     conn.release();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
+
+// (async function createReviewTable() {
+//   try {
+//     const conn = await pool.getConnection();
+
+//     conn.query("CREATE DATABASE IF NOT EXISTS datenight");
+//     conn.query("USE datenight");
+
+//     const reviewDB = await conn.query(
+//       "CREATE TABLE IF NOT EXISTS review (id INT UNIQUE NOT NULL AUTO_INCREMENT, username VARCHAR(255) UNIQUE NOT NULL, date VARCHAR(255) NOT NULL, actitivity_location VARCHAR(255) NOT NULL, price VARCHAR(255) NOT NULL, PRIMARY KEY(id) )"
+//     );
+//     console.log(reviewDB);
+//     conn.release();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
